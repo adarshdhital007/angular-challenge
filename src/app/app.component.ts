d4c0bc3b1d1f8949907aa9dd04d4354073285122
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-12-22 18:17:12 +0545

    refactor : Removed Unused Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { LoginDirtyComponent } from './components/challenges/login-dirty/login-dirty.component';
 import { HttpClientModule } from '@angular/common/http';
-import { DropdownExampleComponent } from './components/challenges/dropdown-example/dropdown-example.component';
-import { CreditCardFormatterComponent } from './components/challenges/credit-card-formatter/credit-card-formatter.component';
-import { TodoWithSignalComponent } from './components/challenges/todo-with-signal/todo-with-signal.component';
-import { Child1Component } from './components/challenges/child/child-1/child-1.component';
-import { Child2Component } from './components/challenges/child/child-2/child-2.component';
+import { MiToastComponent } from './components/shared/common/mi-toast/mi-toast.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,12 +10,8 @@ import { Child2Component } from './components/challenges/child/child-2/child-2.c
     CommonModule,
     RouterOutlet,
     RouterModule,
-    LoginDirtyComponent,
     HttpClientModule,
-    TodoWithSignalComponent,
-    DropdownExampleComponent,
-    CreditCardFormatterComponent,
-    Child1Component,Child2Component
+    MiToastComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],

41bb88e7244e80972f0751beea22ffd1ba7582fc
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-12-27 17:13:30 +0545

    feat : Added Deposit Calculator

---
+++ src/app/components/challenges/deposit-calculator/deposit-calculator.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-deposit-calculator',
+  standalone: true,
+  imports: [CommonModule, FormsModule,ReactiveFormsModule],
+  templateUrl: './deposit-calculator.component.html',
+  styleUrl: './deposit-calculator.component.scss',
+  host: { ngSkipHydration: 'true' },
+})
+export class DepositCalculatorComponent implements OnInit {
+  initialDeposit = 10000;
+  numberOfMonths = 12;
+  interestRate = 5;
+
+  moneyBefore = '';
+  moneyAfter = '';
+  heightAfter = 0;
+
+  constructor() {}
+  ngOnInit(): void {
+    this.calculate();
+  }
+
+  calculate(): void {
+    if (!this.initialDeposit) return;
+
+    const interest = this.interestRate * 0.01;
+
+    if (!interest) return;
+    const years = this.numberOfMonths / 12;
+
+    if (!years) return;
+
+    const result = Math.round(this.initialDeposit * (1 + interest) ** years);
+
+    const height = (result / this.initialDeposit) * 100;
+
+    this.heightAfter = height;
+
+    this.moneyBefore = this.initialDeposit.toString();
+
+    this.moneyAfter = result.toString();
+  }
+
+  onInput(): void {
+    this.calculate();
+  }
+
+  onChange(): void {
+    this.calculate();
+  }
+}
